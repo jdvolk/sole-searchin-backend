57b2f0d423044fec227737a6b22a0ff440a2a53a
Alex Eickelman <aeickelman40@gmail.com>
2020-07-23 13:25:36 -0600

    Add knex config files to server.js and add get method for those files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 
 app.set('port', process.env.PORT || 3002)
 
@@ -10,4 +14,15 @@ app.listen(app.get('port'), () => {
 
 app.get('/', (request, response) => {
     response.send('SOLE SEARCHIN')
+});
+
+app.get('/api/v1/shoes', async (request, response) => {
+    try {
+        const shoes = await database('shoes').select();
+        response.status(200).json(shoes);
+    } catch (error) {
+        response.status(500).json({
+            error
+        });
+    }
 });
