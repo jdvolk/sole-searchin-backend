5f5f45be7e35d761ca0e595f1ac23640b3b4dcee
jdvolk <justin.d.volk@gmail.com>
2020-07-25 17:02:08 -0600

    changed path to knex

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(cors())
 app.use(express.json())
 
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
+const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 
