06c323b9510ee4c6622cf037cc3769f7ad5c8133
Alex Eickelman <aeickelman40@gmail.com>
2020-07-28 15:11:15 -0600

    Finalize post logic

--- server/server.js
+++ server/server.js
@@ -105,22 +105,5 @@ app.post('/api/v1/shoes', async (request, response) => {
     }
 })
 
-    //         "id": 1018,
-    //         "brand": "Vans",
-    //         "model": "Vans Sk8-Lo Re-Issue",
-    //         "product": "Taka Hayashi QR Checkerboard Blue",
-    //         "title": "Vans Sk8-Lo Re-Issue Taka Hayashi QR Checkerboard Blue",
-    //         "colorway": "Total Eclipse",
-    //         "demographic": "men",
-    //         "retail_price": 200,
-    //         "year": 2020,
-    //         "release_date": "2020-06-10T23:59:59.000Z",
-    //         "image_url": "https://stockx-360.imgix.net/Vans-Sk8-Lo-Re-Issue-Taka-Hayashi-QR-Checkerboard-Blue.png?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1591852554",
-    //         "small_image_url": "https://stockx-360.imgix.net/Vans-Sk8-Lo-Re-Issue-Taka-Hayashi-QR-Checkerboard-Blue.png?fit=fill&bg=FFFFFF&w=300&h=214&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1591852554",
-    //         "thumb_url": "https://stockx-360.imgix.net/Vans-Sk8-Lo-Re-Issue-Taka-Hayashi-QR-Checkerboard-Blue.png?fit=fill&bg=FFFFFF&w=140&h=100&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1591852554",
-    //         "created_at": "2020-07-27T19:59:57.402Z",
-    //         "updated_at": "2020-07-27T19:59:57.402Z"
-    //   })
-
 
 module.exports = app
